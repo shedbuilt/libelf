a45e8e2be32a2c4f618d2ebfd53c5c54d58fc47d
Auston Stewart <auston@shedbuilt.net>
2018-05-30 09:40:12 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-./configure --prefix=/usr || return 1
-make -j $SHED_NUM_JOBS || return 1
-make DESTDIR="$SHED_FAKE_ROOT" -C libelf install || return 1
+# Patch for GCC 8.x compilation
+sed -e '/ALIGN_PRSTATUS)/{
+        s/__attribute/attribute_packed &/
+        s/packed, //}' \
+    -i backends/linux-core-note.c &&
+# Configure
+./configure --prefix=/usr &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" -C libelf install &&
 install -v -Dm644 config/libelf.pc "${SHED_FAKE_ROOT}/usr/lib/pkgconfig/libelf.pc"
