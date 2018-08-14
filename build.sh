584587bce4a1a2e3fe7b0703317182d6622df955
Auston Stewart <auston@shedbuilt.net>
2018-08-14 12:12:05 -1000

    Updating to elfutils 0.173

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Patch for GCC 8.x compilation
-sed -e '/ALIGN_PRSTATUS)/{
-        s/__attribute/attribute_packed &/
-        s/packed, //}' \
-    -i backends/linux-core-note.c &&
 # Configure
 ./configure --prefix=/usr &&
 # Build and Install
